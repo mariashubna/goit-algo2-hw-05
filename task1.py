8a01fe713800d7dd8675fcdbc86c6b30fb36d708
mariashubna <mariashubna@gmail.com>
2025-05-09 16:57:05 +0200

    created

---
+++ task1.py
@@ -0,0 +1,23 @@
+from probables import BloomFilter
+
+
+def check_password_uniqueness(bloom, new_passwords_to_check):
+    return password, status
+
+
+if __name__ == "__main__":
+    # Ініціалізація фільтра Блума
+    bloom = BloomFilter(size=1000, num_hashes=3)
+
+    # Додавання існуючих паролів
+    existing_passwords = ["password123", "admin123", "qwerty123"]
+    for password in existing_passwords:
+        bloom.add(password)
+
+    # Перевірка нових паролів
+    new_passwords_to_check = ["password123", "newpassword", "admin123", "guest"]
+    results = check_password_uniqueness(bloom, new_passwords_to_check)
+
+    # Виведення результатів
+    for password, status in results.items():
+        print(f"Пароль '{password}' - {status}.")
